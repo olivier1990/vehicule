b106454eb49a3c075e397e09353663fd9f3bda89
Olivier <olivierstordiau@gmail.com>
2020-02-05 17:15:24 +0100

    Update vehicule.blade.php

--- resources/views/vehicule.blade.php
+++ resources/views/vehicule.blade.php
@@ -2,7 +2,7 @@
 
 @section('section')
 <div class="title">
-    <h2>C'est un DINOSAURE {{$variable->marque}}</h2>
+    <h2>C'est une  {{$variable->marque}}</h2>
 </div>
 <p>Notre explication du site. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum malesuada orci facilisis aliquet aliquam. Fusce pellentesque sagittis vulputate. Sed nec risus at neque dignissim vestibulum. Ut pharetra leo sit amet urna pharetra mattis. Donec dapibus dolor sed leo cursus, in euismod magna sagittis. Pellentesque luctus ipsum ac libero pharetra semper. Quisque consequat rutrum lacus vel gravida. Praesent fringilla aliquam pulvinar. Duis blandit dignissim tempor. Praesent finibus felis id augue consequat, sed tincidunt magna tristique. Nullam suscipit mauris mauris. Proin non erat iaculis, efficitur lectus ut, tristique ligula. Vivamus non vehicula massa, nec elementum libero.
 
